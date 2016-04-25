f5af3540d3f7e16dc4751e50ad9ccc12614a885d
execgit <exec@iki.fi>
2016-04-25 22:13:41 +0300

    Fix underlay pages (from graphingwiki)

--- MoinMoin/Page.py
+++ MoinMoin/Page.py
@@ -666,6 +666,17 @@ class Page(object):
             elif includeBackend:
                 return False
 
+            # Look for non-deleted pages only, using get_rev		
+            if not rev and self.rev:		
+               rev = self.rev		
+		
+            if domain is None:		
+                use_underlay = -1		
+            else:		
+                use_underlay = domain == 'underlay'		
+            d, d, exists = self.get_rev(use_underlay, rev)		
+            return exists
+
     def size(self, rev=0):
         """ Get Page size.
 
@@ -1832,7 +1843,7 @@ class RootPage(Page):
 
         return underlay, path
 
-    def getPageList(self, user=None, exists=1, filter=None, include_underlay=True, return_objects=False):
+    def getPageList(self, user=None, exists=1, filter=None, include_underlay=True, return_objects=False, includeBackend=True):
         """ List user readable pages under current page
 
         Currently only request.rootpage is used to list pages, but if we
@@ -1901,7 +1912,7 @@ class RootPage(Page):
                     continue
 
                 # Filter deleted pages
-                if exists and not page.exists():
+                if exists and not page.exists(includeBackend=includeBackend):
                     continue
 
                 # Filter out page user may not read.
