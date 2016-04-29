44976fd49abeb51bba7d434293457c9ae9856644
execgit <exec@iki.fi>
2016-04-29 16:14:54 +0300

    Removed gwiki scripts from setup-MoinMoin.py

--- setup-MoinMoin.py
+++ setup-MoinMoin.py
@@ -197,7 +197,6 @@ def scriptname(path):
 
 # build list of scripts from their implementation modules
 moin_scripts = [scriptname(fn) for fn in glob.glob('MoinMoin/script/[!_]*.py')]
-gwiki_scripts = ['scripts/moin-meta-rehash', 'scripts/moin-editpage']
 
 #############################################################################
 ### Call setup()
@@ -338,7 +337,7 @@ Topic :: Text Processing :: Markup""".splitlines(),
         'build_scripts': build_scripts_moin,
     },
 
-    'scripts': moin_scripts + gwiki_scripts,
+    'scripts': moin_scripts
 
     # This copies the contents of wiki dir under sys.prefix/share/moin
     # Do not put files that should not be installed in the wiki dir, or
