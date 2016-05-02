f418828fa36306b5d9a7dc4dee0448b38391f4f5
Ossi Herrala <oherrala@gmail.com>
2016-05-02 18:20:35 +0300

    Cleanup graphingwiki/__init__.py
     * Move imports to places where they are used (see previous commits)
     * Flake8 fixes
     * Cairo should be cleaned from here also?

--- graphingwiki/__init__.py
+++ graphingwiki/__init__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import re
+from cStringIO import StringIO
+
 from MoinMoin import config
 from MoinMoin.Page import Page
 
-import sys
-import os
-import re
-import socket
-import xmlrpclib
-from cStringIO import StringIO
+try:
+    import cairo
+except ImportError:
+    cairo = None
+
 
 # Get action name for forms
 def actionname(request, pagename=None):
@@ -17,16 +19,11 @@ def actionname(request, pagename=None):
     else:
         return Page(request, pagename).url(request)
 
-def url_unescape(text):
-    return re.sub(r"%([0-9a-f]{2})", lambda mo: chr(int(mo.group(1), 16)), text)
 
 def id_escape(text):
     chr_re = re.compile('[^a-zA-Z0-9-_:.]')
     return chr_re.sub(lambda mo: '_%02x_' % ord(mo.group()), text)
 
-def id_unescape(text):
-    chr_re = re.compile('_([0-9a-f]{2})_')
-    return chr_re.sub(lambda mo: chr(int(mo.group(1), 16)), text)
 
 def values_to_form(values):
     # Form keys are not unicode for some reason
@@ -40,67 +37,15 @@ def values_to_form(values):
         newform[newkey] = oldform[key]
     return newform
 
-# Finding dependencies centrally
 
-pil_found = False
-pil_image = None
+def have_cairo():
+    '''Returns true if cairo is imported successfully
 
-try:
-    from PIL import Image as pil_image
-    pil_found = True
-except ImportError:
-    pass
+    Checking whether cairo can be used should be done by actions and
+    macros using this library.
+    '''
+    return cairo is not None
 
-gv_found = True
-gv = None
-
-try:
-    import gv
-except ImportError:
-    try:
-        sys.path.append('/usr/lib/graphviz/python')
-        sys.path.append('/usr/local/lib/graphviz/python') # OSX
-        sys.path.append('/usr/lib/pyshared/python2.6') # Ubuntu 9.10
-        sys.path.append('/usr/lib/pyshared/python2.5') # Ubuntu 9.10
-        import gv
-    except ImportError:
-        sys.path[-1] = '/usr/lib64/graphviz/python'
-        try:
-            import gv
-        except ImportError:
-            gv_found = False
-
-igraph_found = True
-igraph = None
-
-try:
-    import igraph
-except ImportError:
-    igraph_found = False
-    pass
-
-if gv_found:
-    # gv needs libag to be initialised before using any read methods,
-    # making a graph here seems to ensure aginit() is called
-    gv.graph(' ')
-
-cairo_found = True
-cairo = None
-
-try:
-    import cairo
-except ImportError:
-    cairo_found = False
-    pass
-
-geoip_found = True
-GeoIP = None
-
-try:
-    import GeoIP
-except ImportError:
-    geoip_found = False
-    pass
 
 def cairo_surface_to_png(surface):
     stringio = StringIO()
